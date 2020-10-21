61a88d80184579598be055faef097bd833171acc
Joshua Ramos <bassramos@gmail.com>
2020-10-21 02:12:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function makeInt(string)  {
+    parseInt(string)
+    return parseInt(string, 10)
+}
+function preserveDecimal(string)  {
+    parseFloat(string)
+    return parseFloat(string)
+}
+function add(string)  {
+    return +=(argument1,argument2)
+}
+function subtract(string)   {
+    number -=
+    return -+(string)
+}

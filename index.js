eae17bffa8141681ee2e6b385f5f4cb2432f75d4
Joshua Ramos <bassramos@gmail.com>
2020-10-21 02:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function preserveDecimal(string)  {
     parseFloat(string)
     return parseFloat(string)
 }
-function add(num1, num2)  {
-    return +=(num1, num2)
+function add(a, b)  {
+    return +=(a, b)
 }
